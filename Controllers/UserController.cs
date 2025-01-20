554b99b055fa05e9164f9cdd159712c4ffc1afd8
Вероника <@LAPTOP-MTSG507H>
2025-01-20 20:29:20 +0300

    Добавлена форма для входа Login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -34,6 +34,11 @@ namespace PhotoCommunity2025.Controllers
             }
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Login(string login, string password)
         {
---
+++ Views/User/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoCommunity2025.Views.User
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
